7f558626d31f30d3e40707525df9370e5937f708
Simon Baslé <simonbasle@users.noreply.github.com>
2016-08-26 11:16:11 +0200

    Custom iterator, optimistic mutations
    This change gives CouchbaseArrayList its own Iterator/ListIterator
    supporting the remove method with CAS. All mutation methods that
    return the previous value use the CAS of the previous value as well.
    
    Additionally the list is now properly generic.
    
    Added test for bidi iteration and remove, AbstractList's equivalent of
    clear.

--- src/main/java/com/couchbase/client/commons/CouchbaseArrayList.java
+++ src/main/java/com/couchbase/client/commons/CouchbaseArrayList.java
@@ -2,6 +2,7 @@ package com.couchbase.client.commons;
 
 import com.couchbase.client.core.message.ResponseStatus;
 import com.couchbase.client.core.message.kv.subdoc.multi.Lookup;
+import com.couchbase.client.core.message.kv.subdoc.multi.Mutation;
 import com.couchbase.client.java.Bucket;
 import com.couchbase.client.java.document.JsonArrayDocument;
 import com.couchbase.client.java.document.json.JsonArray;
@@ -14,7 +15,7 @@ import com.couchbase.client.java.subdoc.DocumentFragment;
 import java.util.*;
 
 
-public class CouchbaseArrayList extends AbstractList<Object> {
+public class CouchbaseArrayList<E> extends AbstractList<E> {
 
     private final String id;
     private final Bucket bucket;
@@ -31,7 +32,7 @@ public class CouchbaseArrayList extends AbstractList<Object> {
     }
 
     @Override
-    public Object get(int index) {
+    public E get(int index) {
         String idx = "[" + index + "]";
 
         DocumentFragment<Lookup> result = bucket.lookupIn(id).get(idx).execute();
@@ -39,21 +40,24 @@ public class CouchbaseArrayList extends AbstractList<Object> {
             throw new IndexOutOfBoundsException("Index: " + index);
         }
 
-        return result.content(idx);
+        return (E) result.content(idx);
     }
 
     @Override
     public int size() {
-        return bucket.get(id, JsonArrayDocument.class).content().size();
+        //TODO in Spock, GET_COUNT should be available on subdoc
+        JsonArrayDocument current = bucket.get(id, JsonArrayDocument.class);
+        return current.content().size();
     }
 
     @Override
     public boolean isEmpty() {
-        return bucket.lookupIn(id).exists("[0]").execute().status("[0]") == ResponseStatus.SUBDOC_PATH_NOT_FOUND;
+        DocumentFragment<Lookup> current = bucket.lookupIn(id).exists("[0]").execute();
+        return current.status("[0]") == ResponseStatus.SUBDOC_PATH_NOT_FOUND;
     }
 
     @Override
-    public Object set(int index, Object element) {
+    public E set(int index, E element) {
         if (!JsonValue.checkType(element)) {
             throw new IllegalArgumentException("Unsupported value type.");
         }
@@ -61,11 +65,13 @@ public class CouchbaseArrayList extends AbstractList<Object> {
 
         while(true) {
             try {
-                Object result = bucket.lookupIn(id).get(idx).execute().content(idx);
-                bucket.mutateIn(id).replace(idx, element).execute();
-                return result;
+                DocumentFragment<Lookup> current = bucket.lookupIn(id).get(idx).execute();
+                long returnCas = current.cas();
+                Object result = current.content(idx);
+                bucket.mutateIn(id).replace(idx, element).withCas(returnCas).execute();
+                return (E) result;
             } catch (CASMismatchException ex) {
-
+                //will need to retry get-and-set
             } catch (MultiMutationException ex) {
                 if (ex.firstFailureStatus() == ResponseStatus.SUBDOC_PATH_NOT_FOUND) {
                     throw new IndexOutOfBoundsException("Index: " + index);
@@ -75,7 +81,7 @@ public class CouchbaseArrayList extends AbstractList<Object> {
     }
 
     @Override
-    public void add(int index, Object element) {
+    public void add(int index, E element) {
         if (!JsonValue.checkType(element)) {
             throw new IllegalArgumentException("Unsupported value type.");
         }
@@ -83,18 +89,143 @@ public class CouchbaseArrayList extends AbstractList<Object> {
     }
 
     @Override
-    public Object remove(int index) {
+    public E remove(int index) {
+        String idx = "[" + index + "]";
         while(true) {
             try {
-                bucket.mutateIn(id).remove("[" + index + "]").execute();
-                return true;
+                DocumentFragment<Lookup> current = bucket.lookupIn(id).get(idx).execute();
+                long returnCas = current.cas();
+                Object result = current.content(idx);
+                DocumentFragment<Mutation> updated = bucket.mutateIn(id).remove(idx).withCas(returnCas).execute();
+                return (E) result;
             } catch (CASMismatchException ex) {
-
+                //will have to retry get-and-remove
             } catch (MultiMutationException ex) {
                 if (ex.firstFailureStatus() == ResponseStatus.SUBDOC_PATH_NOT_FOUND) {
                     throw new IndexOutOfBoundsException("Index: " + index);
                 }
             }
+        }
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        //TODO in Spock subdoc may have ARRAY_CONTAINS which can help implement indexOf, remove(Object) and contains
+        return super.contains(o);
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new CouchbaseListIterator(0);
+    }
+
+    @Override
+    public ListIterator<E> listIterator() {
+        return new CouchbaseListIterator(0);
+    }
+
+    @Override
+    public ListIterator<E> listIterator(int index) {
+        return new CouchbaseListIterator(index);
+    }
+
+    @Override
+    public void clear() {
+        //optimized version over AbstractList's (which iterates on all and remove)
+        bucket.upsert(JsonArrayDocument.create(id, JsonArray.empty()));
+    }
+
+    private class CouchbaseListIterator implements ListIterator<E> {
+
+        private long cas;
+        private final ListIterator<E> delegate;
+        //flag set whenever next()/previous() is called, unset on remove()
+        boolean canMutate;
+        //the index to mutate, updated after a next()/previous()
+        int lastVisited;
+        //a flag indicating that remove() was called and lastVisited was already decremented
+        //used by previous() to avoid decrementing the lastVisited index twice
+        boolean hasRemoved;
+
+        public CouchbaseListIterator(int index) {
+            JsonArrayDocument current = bucket.get(id, JsonArrayDocument.class);
+            this.cas = current.cas();
+            this.delegate = ((List<E>) current.content().toList()).listIterator(index);
+            this.lastVisited = -1;
+            this.canMutate = false;
+            this.hasRemoved = false;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return delegate.hasNext();
+        }
+
+        @Override
+        public E next() {
+            lastVisited++;
+            hasRemoved = false;
+            canMutate = delegate.hasNext();
+            return delegate.next();
+        }
+
+        @Override
+        public boolean hasPrevious() {
+            return delegate.hasPrevious();
+        }
+
+        @Override
+        public E previous() {
+            if (!hasRemoved)
+                lastVisited--;
+            hasRemoved = false;
+            canMutate = delegate.hasPrevious();
+            return delegate.previous();
+        }
+
+        @Override
+        public int nextIndex() {
+            return delegate.nextIndex();
+        }
+
+        @Override
+        public int previousIndex() {
+            return delegate.previousIndex();
+        }
+
+        @Override
+        public void remove() {
+            if (!canMutate) {
+                throw new IllegalStateException();
+            }
+            int index = lastVisited;
+            String idx = "[" + index + "]";
+            try {
+                DocumentFragment<Mutation> updated = bucket.mutateIn(id).remove(idx).withCas(this.cas).execute();
+                //update the cas so that several removes in a row can work
+                this.cas = updated.cas();
+                //also correctly reset the state:
+                delegate.remove();
+                this.canMutate = false;
+                this.lastVisited--;
+                this.hasRemoved = true;
+            } catch (CASMismatchException ex) {
+                throw new ConcurrentModificationException("List was modified since iterator creation", ex);
+            } catch (MultiMutationException ex) {
+                if (ex.firstFailureStatus() == ResponseStatus.SUBDOC_PATH_NOT_FOUND) {
+                    throw new ConcurrentModificationException("Element doesn't exist anymore at index: " + index);
+                }
+            }
+        }
+
+        @Override
+        public void set(E e) {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public void add(E e) {
+            throw new UnsupportedOperationException();
         }
     }
 }
--- src/test/java/com/couchbase/client/commons/CouchbaseArrayListTest.java
+++ src/test/java/com/couchbase/client/commons/CouchbaseArrayListTest.java
@@ -3,12 +3,16 @@ package com.couchbase.client.commons;
 import com.couchbase.client.java.Bucket;
 import com.couchbase.client.java.Cluster;
 import com.couchbase.client.java.CouchbaseCluster;
+import com.couchbase.client.java.error.DocumentDoesNotExistException;
+import org.junit.After;
 import org.junit.AfterClass;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.util.Iterator;
 import java.util.List;
+import java.util.ListIterator;
 import java.util.UUID;
 
 import static org.junit.Assert.*;
@@ -29,25 +33,41 @@ public class CouchbaseArrayListTest {
         cluster.disconnect();
     }
 
+    private String uuid;
+
+    @Before
+    public void generateId() {
+        uuid = uuid();
+    }
+
+    @After
+    public void deleteDoc() {
+        try {
+            bucket.remove(uuid);
+        } catch (DocumentDoesNotExistException e) {
+            //ignore
+        }
+    }
+
     private static String uuid() {
         return UUID.randomUUID().toString();
     }
 
     @Test
     public void size() throws Exception {
-        List<Object> list = new CouchbaseArrayList(uuid(), bucket);
+        List<Object> list = new CouchbaseArrayList(uuid, bucket);
         assertEquals(0, list.size());
     }
 
     @Test
     public void isEmpty() throws Exception {
-        List<Object> list = new CouchbaseArrayList(uuid(), bucket);
+        List<Object> list = new CouchbaseArrayList(uuid, bucket);
         assertTrue(list.isEmpty());
     }
 
     @Test
     public void shouldAdd() throws Exception {
-        List<Object> list = new CouchbaseArrayList(uuid(), bucket);
+        List<Object> list = new CouchbaseArrayList(uuid, bucket);
         assertFalse(list.contains("foobar"));
         list.add("foobar");
         assertTrue(list.contains("foobar"));
@@ -57,7 +77,7 @@ public class CouchbaseArrayListTest {
 
     @Test
     public void shouldRemoveByValue() throws Exception {
-        List<Object> list = new CouchbaseArrayList(uuid(), bucket);
+        List<Object> list = new CouchbaseArrayList(uuid, bucket);
         list.add("foo");
         list.add("bar");
         assertTrue(list.contains("foo"));
@@ -83,7 +103,7 @@ public class CouchbaseArrayListTest {
 
     @Test
     public void shouldRemoveByIndex() {
-        List<Object> list = new CouchbaseArrayList(uuid(), bucket);
+        List<Object> list = new CouchbaseArrayList(uuid, bucket);
         list.add("foo");
         list.add("bar");
         list.add("baz");
@@ -97,7 +117,7 @@ public class CouchbaseArrayListTest {
 
     @Test(expected = IndexOutOfBoundsException.class)
     public void shouldThrowOnOutOfBoundsRemove() {
-        List<Object> list = new CouchbaseArrayList(uuid(), bucket);
+        List<Object> list = new CouchbaseArrayList(uuid, bucket);
         list.add("foo");
         list.add("bar");
 
@@ -106,7 +126,7 @@ public class CouchbaseArrayListTest {
 
     @Test
     public void shouldReturnIterator() {
-        List<Object> list = new CouchbaseArrayList(uuid(), bucket);
+        List<Object> list = new CouchbaseArrayList(uuid, bucket);
         list.add("foo");
         list.add("bar");
 
@@ -130,7 +150,7 @@ public class CouchbaseArrayListTest {
 
     @Test
     public void shouldClear() {
-        List<Object> list = new CouchbaseArrayList(uuid(), bucket);
+        List<Object> list = new CouchbaseArrayList(uuid, bucket);
         list.add("foo");
         list.add("bar");
         assertEquals(2, list.size());
@@ -141,9 +161,54 @@ public class CouchbaseArrayListTest {
         assertEquals(0, list.size());
     }
 
+    //since clear is optimized, also test the iterator removal of the whole list
+    @Test
+    public void shouldClearViaIterator() {
+        List<Object> list = new CouchbaseArrayList(uuid, bucket);
+        list.add("foo");
+        list.add("bar");
+        assertEquals(2, list.size());
+
+        Iterator<Object> iterator = list.iterator();
+        while (iterator.hasNext()) {
+            Object next = iterator.next();
+            assertNotNull(next);
+            iterator.remove();
+        }
+
+        assertTrue(list.isEmpty());
+        assertEquals(0, list.size());
+    }
+
+    @Test
+    public void shouldIteratorRemoveInReverseOrder() {
+        List<String> list = new CouchbaseArrayList<String>(uuid, bucket);
+        list.add("keep");
+        list.add("foo");
+        list.add("bar");
+        list.add("baz");
+        list.add("foobar");
+        assertEquals(5, list.size());
+
+        ListIterator<String> iterator = list.listIterator();
+        while (iterator.hasNext()) iterator.next();
+        //at "foobar"
+        iterator.remove(); //-foobar
+        assertEquals("baz", iterator.previous()); //at "baz"
+        assertEquals("bar", iterator.previous()); //at "bar"
+        iterator.remove(); //-bar
+        assertEquals("foo", iterator.previous()); //at "foo"
+        iterator.remove(); //-foo
+        assertEquals("baz", iterator.next()); //at "baz"
+        iterator.remove(); //-baz
+
+        assertEquals(1, list.size());
+        assertEquals("keep", list.get(0));
+    }
+
     @Test
     public void shouldGet() {
-        List<Object> list = new CouchbaseArrayList(uuid(), bucket);
+        List<Object> list = new CouchbaseArrayList(uuid, bucket);
         list.add("foo");
         list.add("bar");
 
@@ -153,7 +218,7 @@ public class CouchbaseArrayListTest {
 
     @Test(expected = IndexOutOfBoundsException.class)
     public void shouldFailOnOutOfBoundsGet() {
-        List<Object> list = new CouchbaseArrayList(uuid(), bucket);
+        List<Object> list = new CouchbaseArrayList(uuid, bucket);
         list.get(4234324);
     }
 
